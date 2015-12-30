05b217d95be9bac02c81fad87facae287fdfde41
Carlis Liu <carlisliu@live.com>
2015-12-30 17:25:01 +0800

    move isStaticFile function to support rewire test

--- middleware/login-filter.js
+++ middleware/login-filter.js
@@ -1,12 +1,13 @@
+var STATIC_URL = /(\.(js|css|jpeg|jpg|png|gif))$/;
 exports = module.exports = function (req, res,next) {
 	if (!req.session || !req.session.user) {
-        if (req.url !== '/users/login' /*&& !isStaticFile()*/) {
+        if (req.url !== '/users/login' && !isStaticFile(req.url)) {
             return res.redirect('/users/login');
         }
     }
     next();
-
-    function isStaticFile (url) {
-        return url && url.test(/(\.[a-zA-Z0-9]+)$/);
-    }
 };
+
+function isStaticFile (url) {
+    return url && STATIC_URL.test(url);
+}
