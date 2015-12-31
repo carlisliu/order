3f2e0e3e4f515b0f693098cb0b4ac25b29d9ee6d
Carlis Liu <carlisliu@live.com>
2015-12-31 10:39:14 +0800

    filter api url

--- middleware/login-filter.js
+++ middleware/login-filter.js
@@ -1,13 +1,20 @@
 var STATIC_URL = /(\.(js|css|jpeg|jpg|png|gif))$/;
-exports = module.exports = function (req, res,next) {
+var API_URL = /\/api\//i;
+
+exports = module.exports = function(req, res, next) {
 	if (!req.session || !req.session.user) {
-        if (req.url !== '/users/login' && !isStaticFile(req.url)) {
-            return res.redirect('/users/login');
-        }
-    }
-    next();
+		var url = req.url;
+		if (url !== '/users/login' && !isStaticFile(url) && !isApi(url)) {
+			return res.redirect('/users/login');
+		}
+	}
+	next();
 };
 
-function isStaticFile (url) {
-    return url && STATIC_URL.test(url);
+function isStaticFile(url) {
+	return url && STATIC_URL.test(url);
+}
+
+function isApi(url) {
+	return url && API_URL.test(url);
 }
