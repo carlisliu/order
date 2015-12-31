7b0b0dc0d252be891d76b8500dd0c05bcf1b7e09
Carlis Liu <carlisliu@live.com>
2015-12-31 11:32:22 +0800

    add cross tracking test case

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  	res.render('index', { title: 'Home' });
+	res.render('index', {
+		title: 'Home'
+	});
 });
+
+router.get('/remote.html', function(req, res) {
+	remote(res);
+});
+
+
+function remote(streamObj, done) {
+	var requestOptions = {
+		method: 'GET',
+		host: 'localorder.5lym.com',
+		port: 3000,
+		path: '/api/order'
+	};
+	var request = http.request(requestOptions);
+	request.on('response', function(response) {
+		response.pipe(streamObj);
+		response.on('end', function(argument) {
+			done && done();
+		})
+	});
+	request.on('error', function(error) {
+		console.error(error);
+	});
+	request.end();
+}
 
 module.exports = router;
