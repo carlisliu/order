684925d143696d4519c667bf4457b6f1cdf74155
Carlis Liu <carlisliu@live.com>
2015-12-31 11:31:33 +0800

    add global context accessed by view

---
+++ settings.js
@@ -0,0 +1,12 @@
+var config = require('./config');
+var nodeEnv = process.env.NODE_ENV || 'development';
+var host = config.host[nodeEnv];
+var staticUrl = nodeEnv === 'development' ? '' : 'dist';
+
+exports = module.exports = function (req, res, next) {
+	res.locals.context = {
+		host: host,
+		staticUrl: staticUrl
+	};
+    next();
+};
