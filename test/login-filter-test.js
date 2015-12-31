b94df5a911e0785b0b5b8005f5952a688b2bf87a
Carlis Liu <carlisliu@live.com>
2015-12-31 10:39:34 +0800

    add api url test case

--- test/login-filter-test.js
+++ test/login-filter-test.js
@@ -16,3 +16,18 @@ describe('#isStaticFile', function() {
 		assert.equal(true, isStaticFile('http://localhost:3000/javascripts/sea-modules/jquery/jquery/1.10.1/jquery.js'));
 	});
 });
+
+describe('#isApi', function() {
+	it('isApi should be a function', function() {
+		assert.equal('function', typeof loginFilter.__get__('isApi'));
+	});
+	it('http://localhost:3000/api/order should be a api url', function() {
+		var isApi = loginFilter.__get__('isApi');
+		assert.equal(true, isApi('http://localhost:3000/api/order'));
+	});
+
+	it('http://localhost:3000/api/order/f452fdsf45kkf4fs should be a api url', function() {
+		var isApi = loginFilter.__get__('isApi');
+		assert.equal(true, isApi('http://localhost:3000/api/order/f452fdsf45kkf4fs'));
+	});
+});
