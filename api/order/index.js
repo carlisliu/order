3533776f8daa95d0688cd01f19a2aa1420fb1cf2
carlisliu <carlisliu@gmail.com>
2016-01-02 23:20:44 +0800

    add api method

--- api/order/index.js
+++ api/order/index.js
@@ -3,3 +3,7 @@ var Order = require('../../models').Order;
 exports.findAll = function (callback) {
 	return Order.find(callback);
 };
+
+exports.findById = function (id, callback) {
+	return Order.findOne({_id: id}, callback);
+};
--- routes/api.js
+++ routes/api.js
@@ -2,28 +2,45 @@ var express = require('express');
 var router = express.Router();
 var order = require('../api').order;
 
-router.get('/order', function(req, res) {
-	order.findAll(function(error, orders) {
-		if (error) {
-			return res.json({
-				status: 'error',
-				message: error.message || '',
-				error: error
-			});
-		}
-		res.json({
-			status: 'success',
-			message: 'query ok.',
-			data: orders
-		});
-	});
+router.get('/order', function (req, res) {
+    order.findAll(function (error, orders) {
+        if (error) {
+            return res.json({
+                status: 'error',
+                message: error.message || '',
+                error: error
+            });
+        }
+        res.json({
+            status: 'success',
+            message: 'query ok.',
+            data: orders
+        });
+    });
 });
 
-router.get('/order/:id', function(req, res) {
-	res.json({
-		status: 'pending',
-		message: 'not implemented yet.'
-	});
+router.get('/order/:id', function (req, res) {
+    var id = req.param('id');
+    if (id) {
+        return order.findById(id, function (error, order) {
+            if (error) {
+                return res.json({
+                    status: 'error',
+                    message: error.message || '',
+                    error: error
+                });
+            }
+            res.json({
+                status: 'success',
+                message: 'query ok.',
+                data: order
+            });
+        });
+    }
+    return res.json({
+        status: 'error',
+        message: 'invalid order id.'
+    });
 });
 
 module.exports = router;
