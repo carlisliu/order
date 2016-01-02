f38323d43c60a40f35765a1460a5c738e42bc05b
carlisliu <carlisliu@gmail.com>
2016-01-03 00:00:05 +0800

    add minify css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-var pkg = require('./package');
+var minifyCss = require('gulp-minify-css');
+//var pkg = require('./package');
 
 
-gulp.task('default', function() {
-	/*gulp.src('src/src.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/'));*/
-	console.log('default');
+gulp.task('default', function () {
+    /*gulp.src('src/src.js')
+     .pipe(uglify())
+     .pipe(gulp.dest('dist/'));*/
+
+
+});
+
+gulp.task('minify-css', function () {
+    return gulp.src('public/stylesheets/*.css')
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('public/dist'));
 });
