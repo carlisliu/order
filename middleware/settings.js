4ce4c9c809ffcce9b324f0cd2c02d68683ce4098
Carlis Liu <carlisliu@live.com>
2016-01-04 18:07:12 +0800

    move setting

---
+++ middleware/settings.js
@@ -0,0 +1,13 @@
+var config = require('./config');
+var nodeEnv = process.env.NODE_ENV || 'development';
+var host = config.host[nodeEnv];
+var staticUrl = nodeEnv === 'development' ? '' : '/dist';
+
+exports = module.exports = function (req, res, next) {
+	res.locals.context = {
+		nodeEnv: nodeEnv,
+		host: host,
+		staticUrl: staticUrl
+	};
+    next();
+};
