7d5913ce79dd80bf72fec335a8dc5d83b5a8d603
Carlis Liu <carlisliu@live.com>
2016-01-04 18:11:50 +0800

    fix path

--- middleware/settings.js
+++ middleware/settings.js
@@ -1,4 +1,4 @@
-var config = require('./config');
+var config = require('../config');
 var nodeEnv = process.env.NODE_ENV || 'development';
 var host = config.host[nodeEnv];
 var staticUrl = nodeEnv === 'development' ? '' : '/dist';
