f3755db5c454becb3bc6fc5021d3187765f5d2ad
Carlis Liu <carlisliu@live.com>
2016-01-04 16:15:47 +0800

    change tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,72 @@ var minifyCss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var clean = require('gulp-clean');
 
-gulp.task('css', ['clean-css'], function () {
+gulp.task('css', function() {
     gulp.src(['public/stylesheets/bootstrap.css', 'public/stylesheets/bootstrap-responsive.css',
-            'jquery.fancybox.css', 'public/stylesheets/style.css'])
-        .pipe(minifyCss({compatibility: 'ie8'}))
+            'jquery.fancybox.css', 'public/stylesheets/style.css'
+        ])
+        .pipe(minifyCss({
+            compatibility: 'ie8'
+        }))
         .pipe(concat('all.css'))
         .pipe(gulp.dest('public/dist/stylesheets'));
+});
 
+gulp.task('css-login', function() {
     gulp.src('public/stylesheets/login/*.css')
-        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(minifyCss({
+            compatibility: 'ie8'
+        }))
         .pipe(concat('login.all.css'))
-        .pipe(gulp.dest('public/dist/stylesheets/login'));
+        .pipe(gulp.dest('public/dist/stylesheets/login')).on('task_err', function(err) {
+            console.log(err);
+        }).on('error', function (err) {
+             console.log(err);
+        });
 });
 
-gulp.task('clean-css', function () {
-    gulp.src('public/dist/stylesheets/*', {read: false})
+gulp.task('clean-css-login', function() {
+    gulp.src('public/dist/stylesheets/login')
+        .pipe(clean()).on('task_err', function(err) {
+            console.log(err);
+        }).on('error', function (err) {
+             console.log(err);
+        });
+});
+
+gulp.task('clean-css', function() {
+    gulp.src('public/dist/stylesheets/*')
         .pipe(clean());
 });
 
-gulp.task('clean-script', function () {
-    gulp.src('public/dist/javascripts/static/*', {read: false})
+gulp.task('clean-static', function() {
+    gulp.src(['public/dist/javascripts/static/*'])
         .pipe(clean());
 });
 
-gulp.task('script', ['clean-script'], function () {
+gulp.task('clean-modules', function() {
+    gulp.src(['public/dist/javascripts/sea-modules/bootstrap/*'])
+        .pipe(clean());
+});
+
+gulp.task('script-static', ['clean-static'], function() {
     gulp.src('public/javascripts/static/**/*.js')
         .pipe(concat('all.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('public/dist/javascripts/static'));
 });
 
-gulp.task('default', ['script', 'css']);
+gulp.task('script-modules', function() {
+    gulp.src(['public/javascripts/sea-modules/**/**/*.js', 
+            '!public/javascripts/sea-modules/backstretch/**/*.js', 
+            '!public/javascripts/sea-modules/seajs/**/*.js', 
+            'public/javascripts/sea-modules/**/**/**/*.js',
+            '!public/javascripts/sea-modules/jquery/jquery/**/*.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('public/dist/javascripts/sea-modules/')).on('error', function(error) {
+            console.log(error);
+        });
+
+});
+
+gulp.task('default', ['script-static', 'script-modules', 'css', 'css-login']);
