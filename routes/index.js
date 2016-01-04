eab0635138f22aa4cd61b526402c15d35ac87003
Carlis Liu <carlisliu@live.com>
2016-01-04 09:56:45 +0800

    remote case

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var http = require('http');
+var Customer = require('../proxy').Customer;
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -13,16 +14,39 @@ router.get('/remote.html', function(req, res) {
 	remote(res);
 });
 
+router.get('/remote-node.html', function(req, res) {
+	Customer.findAllCustomers(function (error, customers) {
+		console.log(customers);
+		remote(res);
+	});
+});
+
+router.get('/remote-order.html', function(req, res) {
+	Customer.findAllCustomers(function (error, customers) {
+		console.log(customers);
+		remote(res);
+	});
+});
+
+router.get('/remote-mysql.html', function(req, res) {
+	res.setHeader("Content-Type", "application/json;charset=utf-8");
+	Customer.findAllCustomers(function (error, customers) {
+		console.log(customers);
+		remote(res);
+	});
+});
+
 
 function remote(streamObj, done) {
 	var requestOptions = {
 		method: 'GET',
 		host: 'localorder.5lym.com',
-		port: 3000,
-		path: '/api/order'
+		port: 7000,
+		path: '/api/mysql'
 	};
 	var request = http.request(requestOptions);
 	request.on('response', function(response) {
+		response.setEncoding('utf8');
 		response.pipe(streamObj);
 		response.on('end', function(argument) {
 			done && done();
