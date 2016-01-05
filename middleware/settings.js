df2766df349dd931ac3cf5eea768200eba0d44ca
Carlis Liu <carlisliu@live.com>
2016-01-05 16:34:52 +0800

    modify staticUrl

--- middleware/settings.js
+++ middleware/settings.js
@@ -1,11 +1,14 @@
 var config = require('../config');
-var nodeEnv = process.env.NODE_ENV || 'development';
-var host = config.host[nodeEnv];
-var staticUrl = nodeEnv === 'development' ? '' : '/dist';
+var url = require('url');
+
+var host = config.host;
+var staticServer = config.staticServer;
+var staticResourcePath = config.staticResourcePath || '/';
+var staticUrl = url.resolve(staticServer, staticResourcePath);
 
 exports = module.exports = function (req, res, next) {
 	res.locals.context = {
-		nodeEnv: nodeEnv,
+		nodeEnv: process.env.NODE_ENV || 'development',
 		host: host,
 		staticUrl: staticUrl
 	};
