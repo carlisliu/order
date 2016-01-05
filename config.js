2192e1258dda5560bf2c9b135d34cee437ec4167
Carlis Liu <carlisliu@live.com>
2016-01-05 16:34:30 +0800

    change config struction

--- config.js
+++ config.js
@@ -1,29 +1,47 @@
 /**
  * Created by Carlis on 4/8/15.
  */
-
-var db = {
-    ip: '127.0.0.1',
-    schema: 'order',
-    username: 'root',
-    password: 'root'
-};
-//'mongodb://127.0.0.1/order'
-db.url = 'mongodb://' + db.ip + ':' + (db.port || 27017) + '/' + db.schema;
-//mongodb://root:root@localhost:27017/order
-var dbUri = 'mongodb://' + ((db.username && db.password) ? (db.username + ':' + db.password + '@') : '') + db.ip + ':' + (db.port || 27017) + '/' + db.schema;
 var config = {
-    db: db,
-    dbUri: dbUri,
-    productUploadPath: '/upload/products',
-    staticServer: '',
-    page: {
-        pageSize: 10,
-        paginationSize: 5
+    'development': {
+        db: {
+            ip: '127.0.0.1',
+            schema: 'order',
+            username: 'root',
+            password: 'root'
+        },
+        productUploadPath: '/upload/products',
+        staticServer: 'http://localhost:3000',
+        staticResourcePath: '/',
+        page: {
+            pageSize: 10,
+            paginationSize: 5
+        },
+        host: 'http://localhost:3000'
     },
-    host: {
-        production: 'http://order.5lym.com',
-        development: ''
+    'production': {
+        db: {
+            ip: '127.0.0.1',
+            schema: 'order',
+            username: 'root',
+            password: 'root'
+        },
+        productUploadPath: '/upload/products',
+        staticServer: 'http://static.5lym.com',
+        staticResourcePath: '/dist',
+        page: {
+            pageSize: 10,
+            paginationSize: 5
+        },
+        host: 'http://order.5lym.com'
     }
 };
-module.exports = config;
+
+for (var key in config) {
+    var db = config[key].db;
+    //'mongodb://127.0.0.1/order'
+    db.url = 'mongodb://' + db.ip + ':' + (db.port || 27017) + '/' + db.schema;
+    //mongodb://root:root@localhost:27017/order
+    config[key].dbUri = 'mongodb://' + ((db.username && db.password) ? (db.username + ':' + db.password + '@') : '') + db.ip + ':' + (db.port || 27017) + '/' + db.schema;
+}
+
+module.exports = config[process.env.NODE_ENV || 'development'];
