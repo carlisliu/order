cc7b3413aa4f4c80bbf445b0b3dd1ed28537fea6
Carlis Liu <carlisliu@live.com>
2016-01-06 17:19:29 +0800

    add local context

--- middleware/settings.js
+++ middleware/settings.js
@@ -1,16 +1,19 @@
 var config = require('../config');
 var url = require('url');
+var resources = require('./resources');
 
 var host = config.host;
 var staticServer = config.staticServer;
 var staticResourcePath = config.staticResourcePath;
 var staticUrl = url.resolve(staticServer, staticResourcePath);
 
+
 exports = module.exports = function (req, res, next) {
 	res.locals.context = {
 		nodeEnv: process.env.NODE_ENV || 'development',
 		host: host,
-		staticUrl: staticUrl
+		staticUrl: staticUrl,
+		resources: resources
 	};
     next();
 };
