4855c5997dd7f62565919e59d019891625c31b16
Carlis Liu <carlisliu@live.com>
2016-01-08 19:15:03 +0800

    add user proxy methods

--- proxy/user.js
+++ proxy/user.js
@@ -1,4 +1,5 @@
 var User = require('../models').User;
+var _ = require('lodash');
 
 exports.findUserById = function(id, callback) {
 	return User.findOne({
@@ -6,6 +7,30 @@ exports.findUserById = function(id, callback) {
 	}, callback);
 };
 
-exports.updateUser = function (id, userProps, callback) {
-	return User.update({id: id}, {'$set': userProps}, callback);
+exports.saveUser = function(user, callback) {
+	var _user = new User();
+	_.assign(_user, user);
+	_user.save(function(error) {
+		callback(error);
+	});
+};
+
+exports.findUsers = function(params, callback) {
+	User.find(params, callback);
+};
+
+exports.findOneUser = function(params, callback) {
+	User.findOne(params, callback);
+};
+
+exports.removeUser = function(params, callback) {
+	User.remove(params, callback);
+};
+
+exports.updateUser = function(id, userProps, callback) {
+	return User.update({
+		id: id
+	}, {
+		'$set': userProps
+	}, callback);
 };
