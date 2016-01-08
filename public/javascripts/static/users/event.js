d74316a3c5ae225e49644c1931922b2b9fe621bb
Carlis Liu <carlisliu@live.com>
2016-01-08 19:19:21 +0800

    implement user page

--- public/javascripts/static/users/event.js
+++ public/javascripts/static/users/event.js
@@ -1,6 +1,62 @@
-define('static/users/event', ['jquery'], function(require) {
+define('static/users/event', ['jquery', 'validate', 'jgrowl', './user', '../common/options', '../common/modal'], function(require) {
 	var $ = require('jquery');
+	var User = require('./user');
+	require('validate');
+	require('jgrowl');
+	var validateOptions = require('../common/options');
+	var Modal = require('../common/modal');
+
+	var DIGIT = /(\d+)/;
+
+	$.validator.setDefaults({
+		onsubmit: false
+	});
+
+	function decreaseTotal () {
+		var totalInfo = $('.dataTables_info');
+		var total = DIGIT.exec(totalInfo.text())[1];
+		total = parseInt(total);
+		if (!isNaN(total) && total > 0) {
+			--total;
+			if (total > 1) {
+				totalInfo.text(total + ' entries');
+			} else {
+				totalInfo.text(total + ' entry');
+			}
+		}
+	}
+
 	$(function() {
-		console.log('loaded.');
+		var user = new User('#user-form');
+
+		$(document.body).on('click', '#save-user', function(e) {
+			e.preventDefault();
+			var form = $('.validate');
+			form.validate(validateOptions);
+			if (form.valid()) {
+				user.post('/users/add.html', function(data) {
+					if (data.status == 'success') {
+						user.clearInput();
+					}
+					$.jGrowl(data.message || 'Saved.');
+				});
+			}
+		}).on('click', 'table#users-holder a.deleteRow', function(e) {
+			e.preventDefault();
+			var $this = $(this);
+			var modal = new Modal('#info-modal');
+			modal.setTitle('Warning').setBody('<p>Do you wanna remove this user?</p>').bindFooter('danger', function() {
+				user.remove($this.attr('data-user-id'), function(data) {
+					if (data.status === 'success') {
+						$this.parents('tr').remove();
+						decreaseTotal();
+						return $.jGrowl(data.message || 'Removed.');
+					}
+					$.jGrowl(data.message || 'Error.');
+				});
+			});
+		}).on('click', 'table#users-holder a.updateRow', function(e) {
+			e.preventDefault();
+		});
 	});
 });
--- public/javascripts/static/users/user.js
+++ public/javascripts/static/users/user.js
@@ -1,8 +1,38 @@
-define('static/users/user', ['jquery'], function(require) {
+define('static/users/user', ['jquery', '../utils/index', '../common/base'], function(require, exports, module) {
 	var $ = require('jquery');
-	
-	function User () {
-	}
+	var util = require('../utils/index');
+	var Base = require('../common/base');
 
-	
+	function User(option) {
+		Base.call(this, option);
+	}
+	util.inherits(User, Base);
+
+	User.prototype.fixData = function(data) {
+		return {
+			user: data
+		};
+	};
+
+	User.prototype.remove = function(id, callback) {
+		this.post(true, '/users/remove.html', {
+			id, id
+		}, callback);
+		return this;
+	};
+
+	User.prototype.clearInput = function() {
+		if (this.container) {
+			this.container.find('input[type=text], input[type=password], input[type=checkbox], select, textarea').each(function() {
+				if (this.type === 'checkbox') {
+					$(this).attr('checked', false);
+				} else {
+					$(this).val('');
+				}
+			});
+		}
+		return this;
+	};
+
+	module.exports = User;
 });
