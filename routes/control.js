f1d66b1f33f0293300dca81a4a4b2adfe4b22e9d
carlisliu <carlisliu@gmail.com>
2016-01-09 20:08:44 +0800

    add control page

--- routes/control.js
+++ routes/control.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/index.html', function(req, res) {
-	res.render('control');
+	res.render('control', {});
 });
 
 module.exports = router;
