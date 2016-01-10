561019ac06177c4e9a014f3bc6bbfa8a931e9ac5
carlisliu <carlisliu@gmail.com>
2016-01-10 22:03:30 +0800

    add control static resources

---
+++ public/javascripts/static/control/control.js
@@ -0,0 +1,10 @@
+define('static/control/control', ['jquery'], function (require, exports, module) {
+	var $ = require('jquery');
+
+	function Control () {
+	}
+
+	Control.prototype = {};
+
+	module.exports = Control;
+});
---
+++ public/javascripts/static/control/event.js
@@ -0,0 +1,7 @@
+define('static/control/event', ['jquery', './control'], function (require, exports, module) {
+	var $ = require('jquery');
+	var Control = require('./control');
+
+	$(function () {
+	});
+});
