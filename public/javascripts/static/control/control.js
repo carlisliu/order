708e6f849ee0b12d5906e311b229f29d5db94a13
Carlis Liu <carlisliu@live.com>
2016-01-11 18:40:28 +0800

    add ctrl implementation

--- public/javascripts/static/control/control.js
+++ public/javascripts/static/control/control.js
@@ -1,10 +1,33 @@
-define('static/control/control', ['jquery'], function (require, exports, module) {
+define('static/control/control', ['jquery'], function(require, exports, module) {
 	var $ = require('jquery');
 
-	function Control () {
+	function Control(record) {
+		this.record = record;
+		this.loading = '/images/loading.gif';
+		this.finished = '/images/action_check.png';
 	}
 
-	Control.prototype = {};
+	Control.prototype = {
+		before: function() {
+			this.record.find('td:last').html(this.render(this.loading));
+			return this;
+		},
+		finish: function(argument) {
+			this.record.find('td:last').html(this.render(this.finished));
+			return this;
+		},
+		render: function(src) {
+			return '<img src="' + src + '" />';
+		},
+		import: function(table, callback) {
+			var that = this;
+			this.before();
+			$.post('/control/table.html', data, function(data) {
+				callback(data);
+				that.finish();
+			});
+		}
+	};
 
 	module.exports = Control;
 });
--- public/javascripts/static/control/event.js
+++ public/javascripts/static/control/event.js
@@ -1,7 +1,61 @@
-define('static/control/event', ['jquery', './control'], function (require, exports, module) {
+define('static/control/event', ['jquery', './control', 'jgrowl', '../common/options', 'validate'], function(require, exports, module) {
 	var $ = require('jquery');
 	var Control = require('./control');
+	require('validate');
+	require('jgrowl');
+	var validateOptions = require('../common/options');
 
-	$(function () {
+	$(function() {
+		$(document.body).on('click', '#tables-holder th input[type=checkbox]', function(e) {
+			var $this = $(this);
+			if ($this.is(':checked')) {
+				$this.parents('table').find('tbody input[type=checkbox]').prop('checked', true);
+			} else {
+				$this.parents('table').find('tbody input[type=checkbox]').prop('checked', false);
+			}
+		}).on('click', '#tables-holder td input[type=checkbox]', function(e) {
+			var $this = $(this);
+			var allChecked = true;
+			if ($this.is(':checked')) {
+				if ($this.attr('data-table') === 'Product') {
+					$this.parents('tr').siblings().find('input[type=checkbox][data-table=Category]').prop('checked', true);
+				}
+				$this.parents('tr').siblings().find('input[type=checkbox]').each(function() {
+					if (!$(this).is(':checked')) {
+						return (allChecked = false);
+					}
+				});
+			} else {
+				if ($this.attr('data-table') === 'Category') {
+					$this.parents('tr').siblings().find('input[type=checkbox][data-table=Product]').prop('checked', false);
+				}
+				allChecked = false;
+			}
+			$this.parents('table').find('thead input[type=checkbox]').prop('checked', allChecked);
+		}).on('click', 'button', function(e) {
+			var form = $('.validate');
+			form.validate(validateOptions);
+			if (!form.valid()) {
+				return;
+			}
+			var source = $('#source').val();
+			var destination = $('#destination').val();
+			if (source === destination) {
+				return $.jGrowl('Source and destination can not be identical.');
+			}
+			$('#tables-holder').find('tbody tr').filter(function() {
+				return $(this).find('input[type=checkbox]').is(':checked');
+			}).each(function() {
+				var $this = $(this);
+				var ctrl = new Control($this);
+				ctrl.import({
+					table: $this.attr('data-table'),
+					source: source,
+					destination: destination
+				}, function(data) {
+					console.log(data);
+				});
+			});
+		});
 	});
 });
