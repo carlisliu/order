fe01995ca4f571fed4743dcf40792c846f7d778e
Carlis Liu <carlisliu@live.com>
2016-01-12 16:35:51 +0800

    add index

--- models/customer.js
+++ models/customer.js
@@ -6,16 +6,46 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var CustomerSchema = new Schema({
-    id: {type: String},
-    company_id: {type: Schema.ObjectId},
-    name: {type: String, unique: true},
-    tel: {type: String},
+    id: {
+        type: String
+    },
+    company_id: {
+        type: Schema.ObjectId
+    },
+    name: {
+        type: String,
+        index: true
+    },
+    tel: {
+        type: String
+    },
     address: {
         street: String,
         city: String,
         country: String
     },
-    create_at: {type: Date, default: Date.now}
+    create_at: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
+
+CustomerSchema.index({
+    company_id: 1,
+    name: 1
+}, {
+    unique: true
+});
+
+CustomerSchema.virtual('fullAddress').get(function() {
+    var result = [this.address.street, this.address.city, this.address.country].filter(function(part) {
+        return !!part;
+    });
+    return result.join(',');
 });
 
 mongoose.model('Customer', CustomerSchema);
--- models/product.js
+++ models/product.js
@@ -3,17 +3,33 @@
  */
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+	Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
-    id: {type: String, unique: true},
-    company_id: {type: Schema.ObjectId},
-    category_id: {type: String},
-    name: {type: String},
-    price: {type: Number},
-    picture_uri: [String],
-    memo: {type: String},
-    create_at: {type: Date, default: Date.now}
+	id: {
+		type: String,
+		unique: true
+	},
+	company_id: {
+		type: Schema.ObjectId
+	},
+	category_id: {
+		type: String
+	},
+	name: {
+		type: String
+	},
+	price: {
+		type: Number
+	},
+	picture_uri: [String],
+	memo: {
+		type: String
+	},
+	create_at: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 mongoose.model('Product', ProductSchema);
