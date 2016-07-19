877fb56550eabecddf27efacbdff62886742e133
Carlis Liu <carlisliu@live.com>
2016-07-19 17:31:11 +0800

    change versions of dependencies

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
+// better performance.
+global.Promise = require('bluebird');
+
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 const serve = require('koa-static');
 const favicon = require('koa-favicon');
 const conditional = require('koa-conditional-get');
 const etag = require('koa-etag');
 const router = require('./router');
 const convert = require('koa-convert');
+const views = require('koa-views');
+const logger = require('koa-logger');
 
 const app = new Koa();
-
-app.use(convert(favicon(__dirname + '/public/favicon.ico')));
+app.use(logger());
+app.use(bodyParser());
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // etag works together with conditional-get
-app.use(convert(conditional()));
-app.use(convert(etag()));
+app.use(conditional());
+app.use(etag());
 
 // or use absolute paths
-app.use(convert(serve(__dirname + '/public')));
+app.use(serve(__dirname + '/public'));
+
+app.use(views(__dirname + '/views', {
+    extension: 'ejs'
+}));
 
 router(app);
 
-app.on('error', function (error, cxt) {
+app.on('error', function(error, cxt) {
     console.error(error);
 });
 
