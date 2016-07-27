b8c0c650f1ecbad61a0664630c731fc10d8924e6
carlisliu <carlisliu@gmail.com>
2016-07-27 23:42:40 +0800

    add user operations

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,28 @@ router.get('/', function(ctx, next) {
     ctx.body = 'users';
 });
 
+router.post('/', async function  (ctx, next) {
+    var user = ctx.user;
+    if (user) {
+        ctx.state = {
+            title: 'koa2 title'
+        };
+
+        return await ctx.render('detail', {});
+    } 
+    await ctx.render('detail');
+});
+
+router.delete('/:id', async function  (ctx, next) {
+    var id = ctx.params.id;
+    if (id) {
+        ctx.state = {
+            title: 'koa2 title'
+        };
+
+        return await ctx.render('detail', {});
+    } 
+    await ctx.render('detail');
+});
+
 module.exports = router;
