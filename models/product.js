52813d0eaa03856a1072e199191623e7a5c0d6fc
carlisliu <carlisliu@gmail.com>
2016-07-31 22:48:31 +0800

    add models

---
+++ models/product.js
@@ -0,0 +1,49 @@
+'use strict';
+
+import mongoose from 'mongoose';
+import uid from 'uid';
+import idValidator from 'mongoose-id-validator';
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  id: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  },
+}, {
+  versionKey: false,
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+  toJSON: {
+    transform(doc, ret) {
+      //delete ret._id;
+      delete ret.hashed_secret;
+    },
+  },
+});
+
+productSchema.plugin(idValidator);
+
+productSchema.pre('validate', function preSave(next) {
+  if (this.isNew) {
+    if (!this.id) this.id = uid(16);
+  }
+  next();
+});
+
+export default mongoose.model('Product', productSchema);
