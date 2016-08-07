6683140cf122453f7f37792846713805d35d6bd8
carlisliu <carlisliu@gmail.com>
2016-08-07 14:32:42 +0800

    add category files

---
+++ routes/category.js
@@ -0,0 +1,64 @@
+import Router from 'koa-router';
+import state from './state';
+import * as categoryService from '../services/category';
+import assert from 'assert';
+
+const router = new Router();
+
+router.get('/', async function (ctx, next) {
+
+});
+
+router.get('/:categoryId', async function (ctx, next) {
+    const categoryId = (ctx.req.query || {}).categoryId;
+
+    if (!categoryId) {
+        return next();
+    }
+
+    const category = await categoryService.findOne({_id: categoryId});
+
+    await ctx.render('category/detail', state({
+        title: 'Category detail'
+    }));
+});
+
+router.put('/', async function  (ctx, next) {
+    const category = (ctx.body || {}).category;
+
+    validate(category);
+
+    const savedCategory = await categoryService.save(category);
+
+    this.body = state({
+        category: savedCategory
+    });
+});
+
+router.post('/', async function  (ctx, next) {
+    const category = (ctx.body || {}).category;
+
+    validate(category);
+
+    const updatedCategory = await categoryService.update(category);
+
+    this.body = state({
+        category: updatedCategory
+    });
+});
+
+router.delete('/:categoryId', async function  (ctx, next) {
+    const categoryId = (ctx.body || {}).categoryId;
+
+    if (!categoryId) {
+        return next();
+    }
+
+    await categoryService.delete(categoryId);
+
+    this.body = state({
+        categoryId: categoryId
+    });
+});
+
+export router;
